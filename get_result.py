afb10d705f4862cb00f02c0b8367e655516b2ddc
Arthur Leal <50853033+Arthurlrq@users.noreply.github.com>
2021-10-05 19:38:51 -0300

    Delete get_result.py

--- get_result.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import glob
-
-for filename in glob.glob("*.*"):
-    if '.npy' in filename:
-        predicoes = np.load(filename)
-        print(filename)
-
-np.save('predicoes_texto',predicoes)
