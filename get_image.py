c7f63a7b319e77ee5c1584c816a2547b6ac6f080
Arthur Leal <50853033+Arthurlrq@users.noreply.github.com>
2021-10-19 23:45:49 -0300

    Add files via upload

---
+++ get_image.py
@@ -0,0 +1,18 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import matplotlib
+import glob
+
+for filename in glob.glob("*.*"):
+    #if '.npy' in filename:
+    if 'Nxdata' in filename:
+        #print(filename.shape)
+        img_array = np.load(filename)
+        print(img_array.shape)
+        img_array np.squeeze(img_array, axis=3)
+        img_array np.squeeze(img_array, axis=2)
+        plt.imshow(img_array, cmap="gray", vmin=0, vmax=255)
+        plt.show()
+        img_name = filename+".png"
+        matplotlib.image.imsave(img_name, img_array)
+        print(filename)
