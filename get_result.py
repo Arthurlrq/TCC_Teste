5e0b1dbdba323e2bae29601b36b003740bba3cf3
Arthur Leal <arthur.leal.rq@gmail.com>
2021-10-21 15:46:18 -0400

    zxzxzxzx

--- get_result.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import glob
-
-for filename in glob.glob("*.*"):
-    if '.npy' in filename:
-        predicoes = np.load(filename)
-        print(predicoes)
-
-np.savetxt('predicoes_texto',predicoes)
