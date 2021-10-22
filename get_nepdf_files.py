0b75549215abf9d2edc72d25559f2cfd1b57d878
Arthur Leal <50853033+Arthurlrq@users.noreply.github.com>
2021-10-21 23:34:27 -0300

    Add files via upload

---
+++ get_nepdf_files.py
@@ -0,0 +1,29 @@
+import glob
+import numpy as np
+import matplotlib
+from matplotlib import pyplot as plt
+
+for filename in glob.glob("*.*"):
+    if 'Nxdata' in filename:
+        nx_array = np.load(filename)
+        img_array = nx_array[0,:,:,:]
+        img_array = img_array.squeeze()
+        img_name = filename+".png"
+        #
+        plt.imshow(img_array)
+        plt.show()
+        plt.savefig(img_name)
+        #
+        print(filename)
+
+#for filename in glob.glob("*.*"):
+#    if 'ydata' in filename:
+#        labels = np.load(filename)
+#        #print(labels)
+#np.savetxt('labels',labels)
+#
+#for filename in glob.glob("*.*"):
+#    if 'zdata' in filename:
+#        genes = np.load(filename)
+#        #print(genes)
+#np.savetxt('genes',genes)
