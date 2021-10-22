269cb79c0a584b8bf6025f336425a43ea4186c2d
Arthur Leal <50853033+Arthurlrq@users.noreply.github.com>
2021-10-22 15:42:39 -0300

    Delete get_datasets.py

--- get_datasets.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import glob
-
-for filename in glob.glob("*.*"):
-    if '.h5' in filename:
-        dataset = np.load(filename)
-        print(dataset)
-        dataset_name = filename
-        np.savetxt(dataset_name,dataset)
