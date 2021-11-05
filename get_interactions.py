96f2da0a259080ce5519d703892b33009b55b88d
Arthur Leal <50853033+Arthurlrq@users.noreply.github.com>
2021-11-04 22:51:37 -0300

    Add files via upload

---
+++ get_interactions.py
@@ -0,0 +1,13 @@
+#
+
+arquivo_interacoes = open('results.txt')
+interacoes = arquivo_interacoes.readlines()
+
+inter_list = []
+
+for interacao in interacoes:
+	inter = interacao.split()[0]
+	print(inter)
+	print(type(inter))
+	inter_list.append(inter)
+	#print(inter_list)
