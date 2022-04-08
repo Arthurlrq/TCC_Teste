0775332ed175f9636922258a70698619a4a943ca
Arthur Leal <50853033+Arthurlrq@users.noreply.github.com>
2022-04-07 22:44:38 -0300

    Add files via upload

---
+++ get_hub_genes.py
@@ -0,0 +1,70 @@
+from operator import itemgetter
+
+#
+arquivo_pares = open('mmukegg_new_new_unique_rand_labelx.txt')
+pares = arquivo_pares.readlines()
+
+#
+arquivo_genes = open('unique_genes.txt')
+genes_tudo = arquivo_genes.read()
+genes = genes_tudo.splitlines()
+
+#
+arquivo_results = open('results.txt')
+results = arquivo_results.readlines()
+#
+chances = []
+#
+for result in results:
+	chance = result.split()[0]
+	chance = 1 - float(chance)
+	chances.append(chance)
+
+#
+d_genes = {}
+
+#
+print('o numero de pares eh: ')
+print(len(pares))
+
+#
+print('o numero de chances eh: ')
+print(len(chances))
+
+#
+for gene in genes:
+	#d_genes[gene] = {}
+	#d_genes[gene]['Grau'] = 0
+	d_genes[gene] = 0
+	i = 0
+	for par in pares:
+		# SE O OLFR N ESTVER NO PAR ENTAO CONSIDERAR O PAR
+		if 'olfr' not in par:
+			if float(chances[i]) >= 0.8:
+				if gene in par:
+					d_genes[gene] = d_genes[gene] + 1
+		#
+		i = i + 1
+
+#
+d_genes_ordem = {}
+
+#
+d_genes_ordem = {k: v for k, v in sorted(d_genes.items(), key=lambda item: item[1], reverse=True)}
+
+#
+print(d_genes_ordem)
+
+#
+for nome, grau in d_genes_ordem.items():
+	if d_genes_ordem[nome] < 10:
+		del d_genes_ordem[nome]
+
+#
+arquivo_hub = open('hub_genes.txt', 'w')
+
+#
+for nome, grau in d_genes_ordem.items():
+	frase = nome + ' ' + str(grau) + '\n'
+	arquivo_hub.write(frase)
+
