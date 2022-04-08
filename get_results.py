7ca044fa9ab0f7c052a8a207d522f8665d2e87fb
Arthur Leal <arthur.leal.rq@gmail.com>
2022-04-07 22:25:34 -0400

    OWOWOWOWOW'

--- get_results.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import glob
-
-for filename in glob.glob("*.*"):
-    if '.npy' in filename:
-        results = np.load(filename)
-        print(results)
-
-np.savetxt('results.txt',results)
